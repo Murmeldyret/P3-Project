0c41e7c5dddfe4e240b72ac4515d8219edaa66d8
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2022-10-26 10:58:56 +0200

    testAPI
    Co-Authored-By: Rasmus Hald Nielsen <37369606+Murmeldyret@users.noreply.github.com>

--- zenref.Tests/ApiTest.cs
+++ zenref.Tests/ApiTest.cs
@@ -3,11 +3,21 @@ using System.IO;
 using P3Project.API;
 namespace zenref.Tests;
 
+public class TestAPIen : Api
+{
+
+}
+
 public class ApiTest
 {
     [Fact]
     public void Test1()
     {
 
+    }
+    [Fact]
+    public void ReferenceFetchIsNotImplemented()
+    {
+        // Api api = new Api();
     }
 }
