c528bbd1d74d08a46cf746f15f1f180b6eaf6095
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2022-10-28 08:55:26 +0200

    New class for HTTP client

---
+++ zenref/src/Models/ApiHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace zenref.src.Models
+{
+	/// <summary>
+	/// Represents an HTTP client that accepts JSON and XML content
+	/// </summary>
+	public class ApiHelper
+	{
+		/// <summary>
+		/// The actual client making HTTP requests
+		/// </summary>
+		/// <remarks>
+		/// Remember to use the method <c>InitializeClient</c> Before using this property
+		/// </remarks>
+		public static HttpClient ApiClient { get; private set; }
+		public ApiHelper()
+		{
+		}
+
+		public static void InitializeClient()
+		{
+			ApiClient = new HttpClient();
+			ApiClient.DefaultRequestHeaders.Accept.Clear();
+			ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+
+        }
+    }
+}
+
