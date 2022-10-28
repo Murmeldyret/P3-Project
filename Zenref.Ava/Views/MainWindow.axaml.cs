0d74cd3d1e8e0b04d732a246618adb257d774321
KB885 <78407625+KB885@users.noreply.github.com>
2022-10-28 11:58:55 +0200

    Added Avalonia Application

---
+++ Zenref.Ava/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Zenref.Ava.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
