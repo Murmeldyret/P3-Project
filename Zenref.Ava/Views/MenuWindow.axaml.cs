377fc46a97262cea101984dc04d85d73aff9ee02
Casper Christensen <caschr21@student.aau.dk>
2022-10-31 11:08:32 +0100

    Add MenuWindow buttons

--- Zenref.Ava/Views/MenuWindow.axaml.cs
+++ Zenref.Ava/Views/MenuWindow.axaml.cs
@@ -1,12 +1,34 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Zenref.Ava.Views
 {
     public partial class MenuWindow : Window
     {
+        Button KnownReferencesButton;
+        Button IdentifyReferencesButton;
         public MenuWindow()
         {
             InitializeComponent();
+            InitializeWindow();
+        }
+
+        private void InitializeWindow()
+        {
+            KnownReferencesButton = this.FindControl<Button>("knownReferencesButton");
+            KnownReferencesButton.Click += KnownReferencesButton_Click;
+            IdentifyReferencesButton = this.FindControl<Button>("identifyReferencesButton");
+            IdentifyReferencesButton.Click += IdentifyReferencesButton_Click;
+        }
+        private void KnownReferencesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.ShowDialog(this);
+        }
+        private void IdentifyReferencesButton_Click(object sender, RoutedEventArgs e)
+        {
+            DragAndDropWindow dragAndDropWindow = new DragAndDropWindow();
+            dragAndDropWindow.ShowDialog(this);
         }
     }
 }
