eed53d13277999577274fba7a8021c71b3f535a6
Casper Christensen <caschr21@student.aau.dk>
2022-10-31 11:09:24 +0100

    Add buttons to DragAndDropWindow

--- Zenref.Ava/Views/DragAndDropWindow.axaml.cs
+++ Zenref.Ava/Views/DragAndDropWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Zenref.Ava.Models;
 
@@ -7,6 +8,10 @@ namespace Zenref.Ava.Views
 {
     public partial class DragAndDropWindow : Window
     {
+        Button ImportButton;
+        Button CancelButton;
+        Button NextButton;
+
         public DragAndDropWindow()
         {
             this.InitializeComponent();
@@ -20,7 +25,25 @@ namespace Zenref.Ava.Views
 
         private void InitializeWindow()
         {
+            ImportButton = this.FindControl<Button>("importButton");
+            ImportButton.Click += ImportButton_Click;
+            CancelButton = this.FindControl<Button>("cancelButton");
+            CancelButton.Click += CancelButton_Click;
+            NextButton = this.FindControl<Button>("nextButton");
+            NextButton.Click += NextButton_Click;
+        }
+        private void ImportButton_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void NextButton_Click(object sender, RoutedEventArgs e) 
+        {
+            ExportWindow exportWindow = new ExportWindow();
+            exportWindow.ShowDialog(this);
         }
     }
 }
