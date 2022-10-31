c0bd7ddba99c567d239570a0016786f2324bd971
Casper Christensen <caschr21@student.aau.dk>
2022-10-31 10:23:01 +0100

    Add ExportWindow And MenuWindow

---
+++ Zenref.Ava/Views/ExportWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Zenref.Ava.Views
+{
+    public partial class ExportWindow : Window
+    {
+        public ExportWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Zenref.Ava/Views/MenuWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Zenref.Ava.Views
+{
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
