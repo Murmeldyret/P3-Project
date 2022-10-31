184c8d80cabf2d4742258b58c781f15ebf6508bf
Casper Christensen <caschr21@student.aau.dk>
2022-10-31 12:23:30 +0100

    Added buttons for ExportWindow

--- Zenref.Ava/Views/ExportWindow.axaml.cs
+++ Zenref.Ava/Views/ExportWindow.axaml.cs
@@ -1,12 +1,38 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Zenref.Ava.Views
 {
     public partial class ExportWindow : Window
     {
+        Button MenuButton;
+        Button StartButton;
+        Button ExportButton;
         public ExportWindow()
         {
             InitializeComponent();
+            InitializeWindow();
+        }
+        private void InitializeWindow()
+        {
+            MenuButton = this.FindControl<Button>("menuButton");
+            MenuButton.Click += MenuButton_Click;
+            StartButton = this.FindControl<Button>("startButton");
+            ExportButton = this.FindControl<Button>("exportButton");
+
+        }
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuWindow menuWindow = new MenuWindow();
+            menuWindow.ShowDialog(this);
+        }
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
