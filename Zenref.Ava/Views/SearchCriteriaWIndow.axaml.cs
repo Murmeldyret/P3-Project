d63bf46bc360ab555b94eb4fa25f131735cf009b
Casper Christensen <caschr21@student.aau.dk>
2022-10-31 12:23:16 +0100

    Added buttons for SearchCriteriaWindow

--- Zenref.Ava/Views/SearchCriteriaWIndow.axaml.cs
+++ Zenref.Ava/Views/SearchCriteriaWIndow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Zenref.Ava.Models;
 
@@ -7,21 +8,31 @@ namespace Zenref.Ava.Views
 {
     public partial class SearchCriteriaWindow : Window
     {
+        Button AddButton;
+        Button CancelButton;
         public SearchCriteriaWindow()
         {
-            this.InitializeComponent();
+            InitializeComponent();
 
             InitializeWindow();
         }
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
-
         private void InitializeWindow()
         {
+            AddButton = this.FindControl<Button>("addButton");
+            AddButton.Click += AddButton_Click;
+            CancelButton = this.FindControl<Button>("cancelButton");
+            CancelButton.Click += CancelButton_Click;
 
+
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
