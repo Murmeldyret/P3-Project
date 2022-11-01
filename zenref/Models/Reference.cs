04eb47cf1d1978ab8469699bcea8f699e69be295
Erik <erik_easy@hotmail.com>
2022-11-01 13:31:45 +0100

    FuzzyLocal
    Levensthein simplificeret (ren levensthein og ikke effektiv Fastenshtein)

--- zenref/Models/Reference.cs
+++ zenref/Models/Reference.cs
@@ -53,10 +53,58 @@
         public string? Chapters { get; set; }
         public string? BookTitle { get; set; }
         
+        //Based on Levenshteins distance
         public static int FuzzyLocal(string test, string test2)
         {
-            throw new NotImplementedException();
+            /// <summary>
+            /// Compute the distance between two strings.
+            /// </summary>
+            
+                int n = test.Length;
+                int m = test2.Length;
+                int[,] d = new int[n + 1, m + 1];
+
+                // Step 1
+                if (n == 0)
+                {
+                    return m;
+                }
+
+                if (m == 0)
+                {
+                    return n;
+                }
+
+                // Step 2 Initialisering af matrix
+                for (int i = 0; i <= n; d[i, 0] = i++)
+                {
+                }
+
+                for (int j = 0; j <= m; d[0, j] = j++)
+                {
+                }
+
+                // Step 3
+                for (int i = 1; i <= n; i++)
+                {
+                    //Step 4
+                    for (int j = 1; j <= m; j++)
+                    {
+                        // Step 5
+                        int cost = (test2[j - 1] == test[i - 1]) ? 0 : 1;
+
+                        // Step 6
+                        d[i, j] = Math.Min(
+                            Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                            d[i - 1, j - 1] + cost);
+                    }
+                }
+                // Step 7
+                return d[n, m];
         }
+        
+        //    throw new NotImplementedException();
+        //}
 
         public static int FuzzyOnline(string test, string test2)
         {
