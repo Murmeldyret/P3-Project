6b0c9f60d9d2ce6dc78f34e480b99b9fb554ee55
Casper Christensen <caschr21@student.aau.dk>
2022-11-09 11:45:26 +0100

    Added ImportView

---
+++ Zenref.Ava/ViewModels/ImportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Zenref.Ava.ViewModels
+{
+    internal class ImportViewModel : BaseViewModel
+    {
+    }
+}
