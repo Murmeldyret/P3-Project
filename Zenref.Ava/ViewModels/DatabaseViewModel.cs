8bae3a98eb7b0f3d02ba229c946015bdb9ffbacb
Casper Christensen <caschr21@student.aau.dk>
2022-11-09 11:46:07 +0100

    DatabaseView can open ImportView

--- Zenref.Ava/ViewModels/DatabaseViewModel.cs
+++ Zenref.Ava/ViewModels/DatabaseViewModel.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Avalonia.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zenref.Ava.Views;
 
 namespace Zenref.Ava.ViewModels
 {
     internal class DatabaseViewModel : BaseViewModel
     {
+        private void OpenImportView()
+        {
+            Window window = new Window
+            {
+                Title = "Import Window",
+                Content = new ImportView()
+            };
+            window.Show();
+        }
     }
 }
--- Zenref.Ava/Views/DatabaseView.axaml.cs
+++ Zenref.Ava/Views/DatabaseView.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Zenref.Ava;
 
 namespace Zenref.Ava.Views
 {
