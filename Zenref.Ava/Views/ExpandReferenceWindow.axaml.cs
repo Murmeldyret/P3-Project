544c8020b0ac13494619b578929eb20fc61f887e
Casper Christensen <caschr21@student.aau.dk>
2022-11-10 12:13:19 +0100

    Implementation of ExpandReferenceWindow

--- Zenref.Ava/Views/ExpandReferenceWindow.axaml.cs
+++ Zenref.Ava/Views/ExpandReferenceWindow.axaml.cs
@@ -7,20 +7,52 @@ namespace Zenref.Ava.Views
 {
     public partial class ExpandReferenceWindow : Window
     {
-        public ExpandReferenceWindow()
+        public Reference reference;
+        TextBox[] fields = new TextBox[7];
+        public ExpandReferenceWindow(Reference reference)
         {
             InitializeComponent();
+            this.reference = reference;
             InitializeWindow();
         }
-
-        private void InitializeComponent()
+        public ExpandReferenceWindow()
         {
-            AvaloniaXamlLoader.Load(this);
-        }
 
+        }
         private void InitializeWindow()
         {
+            fields[0] = this.FindControl<TextBox>("uddannelseTextBox");
+            if (reference.Uddannelse != null) { fields[0].Text = reference.Uddannelse; }
+            fields[1] = this.FindControl<TextBox>("uddannelsesinstitutionTextBox");
+            if (reference.Uddannelsesinstitution!= null) { fields[1].Text = reference.Uddannelsesinstitution; }
+            fields[2] = this.FindControl<TextBox>("semesterTextBox");
+            if (reference.Semester != null) { fields[2].Text = reference.Semester; }
+            fields[3] = this.FindControl<TextBox>("årTextBox");
+            if (reference.År != null) { fields[3].Text = reference.År; }
+            fields[4] = this.FindControl<TextBox>("pensumlisteTextBox");
+            if (reference.Pensumliste != null) { fields[4].Text = reference.Pensumliste; }
+            fields[5] = this.FindControl<TextBox>("idTextBox");
+            if (reference.Id != null) { fields[5].Text = reference.Id; }
+            fields[6] = this.FindControl<TextBox>("henvisningTextBox");
+            if (reference.Henvisning != null) { fields[6].Text = reference.Henvisning; }
 
+            editButton = this.FindControl<Button>("editButton");
+            editButton.Click += (s, e) =>
+            {
+                reference.Uddannelse = fields[0].Text;
+                reference.Uddannelsesinstitution = fields[1].Text;
+                reference.Semester = fields[2].Text;
+                reference.År = fields[3].Text;
+                reference.Pensumliste = fields[4].Text;
+                reference.Id = fields[5].Text;
+                reference.Henvisning = fields[6].Text;
+                this.Close();
+            };
+            closeButton = this.FindControl<Button>("closeButton");
+            closeButton.Click += (s, e) =>
+            {
+                this.Close();
+            };
         }
     }
 }
