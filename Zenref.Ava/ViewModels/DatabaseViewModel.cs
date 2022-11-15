b93ff71679cff18de96072457bcd872f35178e8d
Casper Christensen <caschr21@student.aau.dk>
2022-11-15 13:41:06 +0100

    Added button functionality to DatabaseView

--- Zenref.Ava/ViewModels/DatabaseViewModel.cs
+++ Zenref.Ava/ViewModels/DatabaseViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Avalonia.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zenref.Ava.Views;
 
 namespace Zenref.Ava.ViewModels
 {
     public class DatabaseViewModel : ViewModelBase
     {
+
+
+
+        private void OpenDragAndDropView(Window window)
+        {
+            DragAndDropView dragAndDropView = new DragAndDropView();
+            dragAndDropView.ShowDialog(window);
+        }
     }
 }
