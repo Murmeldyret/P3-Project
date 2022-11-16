bf34af5d3547d3470db1372fa027615e6493d06f
Casper Christensen <caschr21@student.aau.dk>
2022-11-16 13:11:40 +0100

    Implemented DragAndDrop functionality

--- Zenref.Ava/ViewModels/DragAndDropViewModel.cs
+++ Zenref.Ava/ViewModels/DragAndDropViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
@@ -7,36 +9,22 @@ using Zenref.Ava.Models;
 
 namespace Zenref.Ava.ViewModels
 {
-    public class DragAndDropViewModel : ViewModelBase
+    public partial class DragAndDropViewModel : ObservableObject
     {
-        private string _filePath;
-        public string FilePath
-        {
-            get
-            {
-                return _filePath;
-            }
-            set
-            {
-                _filePath = value;
-                OnPropertyChanged(nameof(FilePath));
-            }
-        }
+        [ObservableProperty]
+        private string filePath;
+
         public DragAndDropViewModel()
         {
-            //this.OpenFileDialogCommand = ReactiveCommand.Create<Window>(FileDialog);
-            //this.CloseWindowCommand = ReactiveCommand.Create<Window>(CloseWindow);
         }
 
 
-        //public ReactiveCommand<Window, Unit> OpenFileDialogCommand { get; }
-        //public ReactiveCommand<Window, Unit> CloseWindowCommand { get; }
-
+        [RelayCommand]
         private async void OpenFileDialog (Window window)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog
             {
-                InitialDirectory = @"C:\",
+                Directory = @"C:\",
                 Title = "Browse for excel file",
                 AllowMultiple = true,
             };
@@ -56,21 +44,5 @@ namespace Zenref.Ava.ViewModels
             }
         }
 
-        private void DragOver(object sender, DragEventArgs e)
-        {
-            e.DragEffects = e.DragEffects & DragDropEffects.Copy;
-            if (!e.Data.Contains(DataFormats.FileNames))
-            {
-                e.DragEffects = DragDropEffects.None;
-            }
-        }
-        private void FileDrop(object sender, DragEventArgs e)
-        {
-            if (e.Data.Contains(DataFormats.FileNames))
-            {
-                string filePath = string.Join(Environment.NewLine, e.Data.GetFileNames());
-                Debug.WriteLine(filePath);
-            }
-        }
     }
 }
--- Zenref.Ava/Views/DragAndDropView.axaml.cs
+++ Zenref.Ava/Views/DragAndDropView.axaml.cs
@@ -1,14 +1,38 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using System.Diagnostics;
+using System;
 using Zenref.Ava.ViewModels;
 
 namespace Zenref.Ava.Views
 {
     public partial class DragAndDropView : Window
     {
+        DragAndDropViewModel dragAndDropViewModel = new DragAndDropViewModel();
         public DragAndDropView()
         {
             InitializeComponent();
-            DataContext = new DragAndDropViewModel();
+            DataContext = dragAndDropViewModel;
+            AddHandler(DragDrop.DropEvent, FileDrop);
+            AddHandler(DragDrop.DragOverEvent, DragOver);
+        }
+
+        private void DragOver(object sender, DragEventArgs e)
+        {
+            e.DragEffects = e.DragEffects & DragDropEffects.Copy;
+            if (!e.Data.Contains(DataFormats.FileNames))
+            {
+                e.DragEffects = DragDropEffects.None;
+            }
+        }
+        private void FileDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.Contains(DataFormats.FileNames))
+            {
+                string filePath = string.Join(Environment.NewLine, e.Data.GetFileNames());
+                dragAndDropViewModel.FilePath = filePath;
+                Debug.WriteLine(filePath);
+            }
         }
     }
 }
