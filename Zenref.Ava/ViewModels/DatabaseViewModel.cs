ebf63951ffb57076e91d4033bd8420c81dbad6ca
Casper Christensen <caschr21@student.aau.dk>
2022-11-16 13:21:40 +0100

    Implemented ability to search references in UI

--- Zenref.Ava/ViewModels/DatabaseViewModel.cs
+++ Zenref.Ava/ViewModels/DatabaseViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using CommunityToolkit.Mvvm.ComponentModel;
+using DynamicData;
 using DynamicData.Binding;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Zenref.Ava.Models;
@@ -11,24 +17,44 @@ using Zenref.Ava.Views;
 
 namespace Zenref.Ava.ViewModels
 {
-    public class DatabaseViewModel : ViewModelBase
+    public partial class DatabaseViewModel : ObservableObject
     {
-        private ObservableCollection<Reference> _references;
+        [ObservableProperty]
+        private ObservableCollection<Reference> references;
+        [ObservableProperty]
+        private IEnumerable<Reference> filteredReferences;
 
-        public ObservableCollection<Reference> References => _references;
         public DatabaseViewModel()
         {
-            string s = "test";
-            int i = 1;
-            double d = 0.2;
-            _references = new ObservableCollection<Reference>();
-            _references.Add(new Reference(s, s, s, s, i, i, s, s, s, s, i, d, s, s, s, s, s, i, s, s, s, s));
+            // FOR TESTING DATAGRID DISPLAYING REFERENCES
+            //references = new ObservableCollection<Reference>();
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    List<string> s = new List<string>();
+            //    for (int k = 0; k < 20; k++)
+            //    {
+            //        s.Add(RandomString(10));
+            //    }
+            //    double d = 0.5;
+            //    references.Add(new Reference(s[0], s[1], s[2], s[3], i, i, s[4], s[5], s[6], s[7], i, d, s[8], s[9], s[10], s[11], s[12], i, s[13], s[14], s[15], s[16]));
+            //}
+            //filteredReferences = references;
         }
+
+        // FOR TESTING DATAGRID DISPLAYING REFERENCES
+        //private static Random random = new Random();
+        //public static string RandomString(int length)
+        //{
+        //    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //    return new string(Enumerable.Repeat(chars, length)
+        //        .Select(s => s[random.Next(s.Length)]).ToArray());
+        //}
 
         private void OpenDragAndDropView(Window window)
         {
             DragAndDropView dragAndDropView = new DragAndDropView();
             dragAndDropView.ShowDialog(window);
         }
+
     }
 }
--- Zenref.Ava/Views/DatabaseView.axaml.cs
+++ Zenref.Ava/Views/DatabaseView.axaml.cs
@@ -1,4 +1,9 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Zenref.Ava.Models;
 using Zenref.Ava.ViewModels;
 
 namespace Zenref.Ava.Views
@@ -10,6 +15,20 @@ namespace Zenref.Ava.Views
         {
             InitializeComponent();
             DataContext = databaseViewModel;
+        }
+
+        private void SearchFilter(object sender, KeyEventArgs e)
+        {
+            TextBox? textBox = sender as TextBox;
+            if (textBox?.Text != "")
+            {
+                var filteredList = databaseViewModel.References.Where(x => x.Author.ToLower().Contains(textBox.Text.ToLower()));
+                databaseViewModel.FilteredReferences = filteredList;
+            }
+            else
+            {
+                databaseViewModel.FilteredReferences = databaseViewModel.References;
+            }
         }
     }
 }
