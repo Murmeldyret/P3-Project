6eb0638429ed4d35fe84e65cdfd0ad161c2d3b3b
Casper Christensen <caschr21@student.aau.dk>
2022-12-02 12:02:36 +0100

    Outcommented test and added small descriptions

--- Zenref.Ava/ViewModels/DatabaseViewModel.cs
+++ Zenref.Ava/ViewModels/DatabaseViewModel.cs
@@ -4,7 +4,6 @@ using CommunityToolkit.Mvvm.Input;
 using MessageBox.Avalonia.BaseWindows.Base;
 using MessageBox.Avalonia.DTO;
 using MessageBox.Avalonia.Models;
-using MessageBox.Avalonia.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -30,40 +29,43 @@ namespace Zenref.Ava.ViewModels
         public DatabaseViewModel()
         {
             // FOR TESTING DATAGRID DISPLAYING REFERENCES
-            references = new ObservableCollection<Reference>();
-            references.Add(new Reference("J.K. Rowling", "Harry Potter and the Philosopher's Stone", "Bog", "Bloomsbury", 1997, 10256358, "How to magic", "Hogwarts", "5. Semester", "Engelsk", 2022, 0.8, "Magi", "How to wave a wand", "Forår", "Magic for beginners", "DanBib", 223, "Hvem ved", "Quidditch", "Harry Potter and the Philosopher's Stone", "Rowling, J. K. (1997). Harry Potter and the Philosopher’s Stone (1st ed.). Bloomsbury."));
-            for (int i = 0; i < 20; i++)
-            {
-                List<string> s = new List<string>();
-                for (int k = 0; k < 20; k++)
-                {
-                    s.Add(RandomString(10));
-                }
-                double d = 0.5;
-                references.Add(new Reference(s[0], s[1], s[2], s[3], i, i, s[4], s[5], s[6], s[7], i, d, s[8], s[9], s[10], s[11], s[12], i, s[13], s[14], s[15], s[16]));
-            }
-            filteredReferences = references;
+            //references = new ObservableCollection<Reference>();
+            //references.Add(new Reference("J.K. Rowling", "Harry Potter and the Philosopher's Stone", "Bog", "Bloomsbury", 1997, 10256358, "How to magic", "Hogwarts", "5. Semester", "Engelsk", 2022, 0.8, "Magi", "How to wave a wand", "Forår", "Magic for beginners", "DanBib", 223, "Hvem ved", "Quidditch", "Harry Potter and the Philosopher's Stone", "Rowling, J. K. (1997). Harry Potter and the Philosopher’s Stone (1st ed.). Bloomsbury."));
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    List<string> s = new List<string>();
+            //    for (int k = 0; k < 20; k++)
+            //    {
+            //        s.Add(RandomString(10));
+            //    }
+            //    double d = 0.5;
+            //    references.Add(new Reference(s[0], s[1], s[2], s[3], i, i, s[4], s[5], s[6], s[7], i, d, s[8], s[9], s[10], s[11], s[12], i, s[13], s[14], s[15], s[16]));
+            //}
+            //filteredReferences = references;
         }
 
         // FOR TESTING DATAGRID DISPLAYING REFERENCES
-        private static Random random = new Random();
-        public static string RandomString(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            return new string(Enumerable.Repeat(chars, length)
-                .Select(s => s[random.Next(s.Length)]).ToArray());
-        }
+        //private static Random random = new Random();
+        //public static string RandomString(int length)
+        //{
+        //    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //    return new string(Enumerable.Repeat(chars, length)
+        //        .Select(s => s[random.Next(s.Length)]).ToArray());
+        //}
         [RelayCommand]
         private void OpenDragAndDropView(Window window)
         {
             DragAndDropView dragAndDropView = new DragAndDropView();
             dragAndDropView.ShowDialog(window);
         }
-
+        /// <summary>
+        /// Method called by button click. The method removes the <paramref name="selectedReference"/> from the collection of references.
+        /// </summary>
+        /// <param name="selectedReference">The reference selected in the datagrid</param>
         [RelayCommand]
         private async void DeleteReference(Reference selectedReference)
         {
-            if(selectedReference != null)
+            if (selectedReference != null)
             {
                 IMsBoxWindow<string> messageBoxCustomWindow = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxCustomWindow(
                     new MessageBoxCustomParams
@@ -77,26 +79,18 @@ namespace Zenref.Ava.ViewModels
                             new ButtonDefinition { Name = "Ja", IsDefault = true }
                         },
                     });
-                if(await messageBoxCustomWindow.Show() == "Ja")
+                if (await messageBoxCustomWindow.Show() == "Ja")
                 {
                     references.Remove(selectedReference);
                 }
-            } 
+            }
         }
-        public async void OnWindowClosing(object sender, CancelEventArgs e)
+        /// <summary>
+        /// Event raised by closing of the window. 
+        /// </summary>
+        public void OnWindowClosing(object sender, CancelEventArgs e)
         {
-            IMsBoxWindow<string> messageBoxCustomWindow = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxCustomWindow(
-            new MessageBoxCustomParams
-            {
-                ContentTitle = "Test MessageBox",
-                ContentMessage = "Du har ugemte ændringer\nVil du lukke uden at gemme?",
-                WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                ButtonDefinitions = new[]
-                {
-                    new ButtonDefinition { Name = "Nej", IsCancel = true },
-                    new ButtonDefinition { Name = "Ja", IsDefault = true }
-                },
-            });
+            // Code to be executed before window is closed.
         }
     }
 }
