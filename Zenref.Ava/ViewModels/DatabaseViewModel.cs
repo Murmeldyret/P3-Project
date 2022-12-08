2d3978c1958b0cb5fe42655ca7e6568bffb1aace
Casper Christensen <caschr21@student.aau.dk>
2022-12-08 12:29:08 +0100

    Housekeeping was here

--- Zenref.Ava/ViewModels/DatabaseViewModel.cs
+++ Zenref.Ava/ViewModels/DatabaseViewModel.cs
@@ -50,35 +50,12 @@ namespace Zenref.Ava.ViewModels
                 {
                     references.Add(reference);
                 }
-                //using (var context = new DataContext())
-                //{
-                //    foreach (Reference reference in inputReferences)
-                //    {
-                //        context.References.Add(reference);
-                //    }
-                //    context.SaveChanges();
-                //}
             });
             using (var context = new DataContext())
             {
                 var referenceList = context.References.ToList();
                 references = new ObservableCollection<Reference>(referenceList);
             }
-            // FOR TESTING DATAGRID DISPLAYING REFERENCES
-            //references = new ObservableCollection<Reference>();
-            //RawReference rawReference = new RawReference("How to magic", "Hogwarts", "5. semester", "1234-4321", "Rowling, J. K. (1997). Harry Potter and the Philosopher’s Stone (1st ed.). Bloomsbury.");
-            //references.Add(new Reference(rawReference, "J.K. Rowling", "Harry Potter and the Philosopher's Stone", "Bog", "Bloomsbury", 1997, "Engelsk", 2022, 0.8, "Kommentar", "How to wave a wand", "Forår", "Magic for beginners", "DanBib", 223, "Hvem ved", "Quidditch", "Bogtitel"));
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    List<string> s = new List<string>();
-            //    for (int k = 0; k < 20; k++)
-            //    {
-            //        s.Add(RandomString(10));
-            //    }
-            //    double d = 0.5;
-            //    RawReference rawReference1 = new RawReference(s[0], s[1], s[2], $"{i}", s[3]);
-            //    references.Add(new Reference(rawReference1, s[4], s[5], s[6], s[7], i, s[8], i, d, s[9], s[10], s[11], s[12], s[13], i, s[14], s[15], s[16]));
-            //}
             filteredReferences = references;
         }
 
@@ -90,32 +67,9 @@ namespace Zenref.Ava.ViewModels
             Debug.WriteLine("Received FilepathsMessage.");
         }
 
-        //FOR TESTING DATAGRID DISPLAYING REFERENCES
-        //private static Random random = new Random();
-        //public static string RandomString(int length)
-        //{
-        //    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        //    return new string(Enumerable.Repeat(chars, length)
-        //        .Select(s => s[random.Next(s.Length)]).ToArray());
-        //}
         [RelayCommand]
         private void OpenDragAndDropView(Window window)
         {
-            using (var context = new DataContext())
-            {
-                try
-                {
-                    RawReference rawReference = new RawReference("How to magic", "Hogwarts", "5. semester", "1234-4321", "Rowling, J. K. (1997). Harry Potter and the Philosopher’s Stone (1st ed.). Bloomsbury.");
-                    Reference testReference = new Reference(rawReference, "J.K. Rowling", "Harry Potter and the Philosopher's Stone", "Bog", "Bloomsbury", 1997, "Engelsk", 2022, 0.8, "Kommentar", "How to wave a wand", "Forår", "Magic for beginners", "DanBib", 223, "Hvem ved", "Quidditch", "Bogtitel");
-                    context.References.Add(testReference);
-                    context.SaveChanges();
-                }
-                catch (DbUpdateException e)
-                {
-                    Debug.WriteLine(e.Data);
-                }
-
-            }
             DragAndDropView dragAndDropView = new DragAndDropView();
             dragAndDropView.ShowDialog(window);
         }
@@ -208,21 +162,21 @@ namespace Zenref.Ava.ViewModels
         public void OnWindowClosing(object sender, CancelEventArgs e)
         {
             // Code to be executed before window is closed.
-            //using (var context = new DataContext())
-            //{
-            //    if (saveChanges)
-            //    {
-            //        foreach (Reference reference in context.References)
-            //        {
-            //            context.References.Remove(reference);
-            //        }
-            //        foreach (Reference reference in References)
-            //        {
-            //            context.References.Add(reference);
-            //        }
-            //        context.SaveChanges();
-            //    }
-            //}
+            using (var context = new DataContext())
+            {
+                if (saveChanges)
+                {
+                    foreach (Reference reference in context.References)
+                    {
+                        context.References.Remove(reference);
+                    }
+                    foreach (Reference reference in References)
+                    {
+                        context.References.Add(reference);
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
